d1e6aaf337b73871b955de9df18a47309c3d5b9d
sivaguruk <sivagurukarthickeyan@gmail.com>
2025-12-04 15:37:29 +0530

    Completion OF Vendor CRUD APIS and Vendor Stats Update

--- src/routes/Vendor.route.ts
+++ src/routes/Vendor.route.ts
@@ -7,5 +7,6 @@ router.get('/',vendorController.getAllVendors)
 router.get('/:id',vendorController.getVendorById)
 router.put('/:id',vendorController.updateVendor)
 router.delete('/:id',vendorController.deleteVendor)
+router.put('/:id/stats', vendorController.updateVendorStats);
 
 export default router
