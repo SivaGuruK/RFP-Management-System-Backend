11fd5b3eea603475179104d1d1251c67312988dd
sivaguruk <sivagurukarthickeyan@gmail.com>
2025-12-04 20:36:08 +0530

    Implementation Of Email Sending And Inbox APIs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import RFProutes from "@/routes/RFP.route"
 import Vendorroutes from "@/routes/Vendor.route"
+import Emailroutes from "@/routes/Email.route"
 
 const app: Express = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/api/v1/rfps', RFProutes);
 app.use('/api/v1/vendors',Vendorroutes)
+app.use('/api/v1/emails',Emailroutes)
 
 export default app
